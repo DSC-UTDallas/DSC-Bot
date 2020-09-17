e738edc6d44c4374593568c7026b3978fdca6413
SaloniSS <sxs170015@utdallas.edu>
2020-09-17 01:49:01 -0500

    send all rules and react in one command

--- commands/react-role.js
+++ commands/react-role.js
@@ -5,11 +5,21 @@ const path = require("path");
 exports.sendRulesReaction = async (msg) => {
   const text = fs.readFileSync(path.resolve(__dirname, "../rules.txt"), "utf8");
   const rules = text.split("\n");
+
   const agreement = [rules[rules.length - 1]];
-  const embed = new Discord.MessageEmbed()
+
+  const embedRules = new Discord.MessageEmbed()
+    .setTitle("Community Guidelines and Rules")
+    .setColor(0x2b85d3)
+    .setDescription(rules.slice(0, 17));
+  msg.channel.send(embedRules);
+
+  const embedAgreement = new Discord.MessageEmbed()
     .setTitle("Agreement to Community Guidelines and Rules")
     .setColor(0x2b85d3)
     .setDescription(agreement);
-  let agreementMsg = await msg.channel.send(embed);
+  let agreementMsg = await msg.channel.send(embedAgreement);
   agreementMsg.react("👍");
+
+  msg.delete({ timeout: 1000 });
 };
