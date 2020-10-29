d1c5a51c95d7614a8bb35e6f8ecb7d55f7afee4e
SaloniSS <sxs170015@utdallas.edu>
2020-10-29 12:46:43 -0500

    Revert update

--- utils/firebase.js
+++ utils/firebase.js
@@ -2,7 +2,7 @@ const firebaseConfig = {
   apiKey: process.env.apiKey,
   authDomain: process.env.authDomain,
   databaseURL: process.env.databaseURL,
-  projectId: "dsc-agenda-bot",
+  projectId: process.env.projectId,
   storageBucket: process.env.storageBucket,
   messagingSenderId: process.env.messagingSenderId,
   appId: process.env.appId,
