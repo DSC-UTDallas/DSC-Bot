03665aaf1449640f521fb348e995ad1de45a9de1
SaloniSS <sxs170015@utdallas.edu>
2021-02-10 23:03:00 -0600

    fix errors

--- commands/message.js
+++ commands/message.js
@@ -1,13 +1,21 @@
 exports.sendMessage = async (msg, client, channelID, message) => {
-  client.channels.cache.get(channelID).send(message);
-  msg.react("👍");
+  const channel = client.channels.cache.get(channelID);
+  if (channel) {
+    channel.send(message);
+    msg.react("👍");
+  } else {
+    msg.reply("Channel does not exist");
+  }
 };
 
 exports.sendDM = async (msg, client, userID, message) => {
-  if (client.users.cache.get(userID).bot) {
+  const user = client.users.cache.get(userID);
+  if (!user) {
+    msg.reply("User does not exist");
+  } else if (user.bot) {
     msg.reply("User is a bot");
   } else {
-    client.users.cache.get(userID).send(message);
+    user.send(message);
     msg.react("👍");
   }
 };
