3e085da3e11453fbedac159858f7b6ea057491d1
Reshmi Ranjith <rreshmiranjith@gmail.com>
2021-03-10 13:14:40 -0600

    gotta remove the logs

--- utils/permissions.js
+++ utils/permissions.js
@@ -1,6 +1,4 @@
 exports.checkPermissions = (msg, role) => {
-  console.log(msg.member.roles.cache);
-  //console.log(role);
   if (
     msg.member.roles.cache.find(
       (r) => r.name.toLowerCase() === role.toLowerCase()
