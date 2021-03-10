6489de1f4f4bfa6af85dfb9593b4fdf479b7e673
Reshmi Ranjith <rreshmiranjith@gmail.com>
2021-03-10 13:12:28 -0600

    oop gotta fix dis too

--- utils/permissions.js
+++ utils/permissions.js
@@ -1,5 +1,11 @@
 exports.checkPermissions = (msg, role) => {
-  if (msg.member.roles.cache.find((r) => r.name.toLowerCase() === role)) {
+  console.log(msg.member.roles.cache);
+  //console.log(role);
+  if (
+    msg.member.roles.cache.find(
+      (r) => r.name.toLowerCase() === role.toLowerCase()
+    )
+  ) {
     return true;
   } else {
     msg.reply("Access denied to command");
