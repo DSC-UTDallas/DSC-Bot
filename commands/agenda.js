81b25daa66611bbb650c59f009c5d232c0a59c53
SaloniSS <sxs170015@utdallas.edu>
2021-03-13 21:40:16 -0600

    agenda logging

--- commands/agenda.js
+++ commands/agenda.js
@@ -1,15 +1,55 @@
 const Discord = require("discord.js");
+const { logger } = require("../utils/logging.js");
 const { POSTidea, GETagenda, DELETEidea } = require("../utils/firebase");
 
 exports.addIdea = async (msg) => {
   const idea = msg.content.substr(msg.content.indexOf(" ") + 1);
-  POSTidea(idea);
+  try {
+    POSTidea(idea);
+    logger.info(
+      msg.author.username +
+        "#" +
+        msg.author.discriminator +
+        " added " +
+        idea +
+        " to officer meeting agenda"
+    );
+  } catch (e) {
+    logger.error(
+      msg.author.username +
+        "#" +
+        msg.author.discriminator +
+        " could not add " +
+        idea +
+        " to officer meeting agenda"
+    );
+    logger.error(e);
+  }
+
   msg.react("👍");
 };
 
 exports.getIdeas = async (msg) => {
   const emoji = msg.guild.emojis.cache.find((emoji) => emoji.name === "dsc");
-  agenda = await GETagenda(emoji);
+  var agenda;
+
+  try {
+    agenda = await GETagenda(emoji);
+    logger.info(
+      msg.author.username +
+        "#" +
+        msg.author.discriminator +
+        " requested officer meeting agenda"
+    );
+  } catch (e) {
+    logger.error(
+      msg.author.username +
+        "#" +
+        msg.author.discriminator +
+        " could not request officer meeting agenda"
+    );
+    logger.error(e);
+  }
 
   const embed = new Discord.MessageEmbed()
     .setTitle("Agenda Ideas for Next Meeting")
@@ -22,6 +62,26 @@ exports.getIdeas = async (msg) => {
 
 exports.deleteIdea = async (msg) => {
   const idea = msg.content.substr(msg.content.indexOf(" ") + 1);
-  DELETEidea(idea);
+  try {
+    DELETEidea(idea);
+    logger.info(
+      msg.author.username +
+        "#" +
+        msg.author.discriminator +
+        " added " +
+        idea +
+        " to officer meeting agenda"
+    );
+  } catch (e) {
+    logger.error(
+      msg.author.username +
+        "#" +
+        msg.author.discriminator +
+        " could not add " +
+        idea +
+        " to officer meeting agenda"
+    );
+    logger.error(e);
+  }
   msg.react("👍");
 };
