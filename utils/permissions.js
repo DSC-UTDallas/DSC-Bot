ce4676b122675a2d3d9c44b0b2c10230bd48c476
SaloniSS <sxs170015@utdallas.edu>
2021-03-13 21:06:41 -0600

    permission roles

--- utils/permissions.js
+++ utils/permissions.js
@@ -1,9 +1,17 @@
+const { logger } = require("./logging.js");
+
 exports.checkPermissions = (msg, role) => {
-  if (
-    msg.member.roles.cache.find(
+  var permsAssigned = false;
+  try {
+    permsAssigned = msg.member.roles.cache.find(
       (r) => r.name.toLowerCase() === role.toLowerCase()
-    )
-  ) {
+    );
+  } catch (e) {
+    const author = msg.author.username + "#" + msg.author.discriminator;
+    logger.error("Error in checking if " + author + " has role " + role);
+    logger.error(e);
+  }
+  if (permsAssigned) {
     return true;
   } else {
     msg.reply("Access denied to command");
