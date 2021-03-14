60bf45397838a791fbf8e34e8048ee8282aee894
SaloniSS <sxs170015@utdallas.edu>
2021-03-13 22:54:02 -0600

    message logger

--- commands/message.js
+++ commands/message.js
@@ -1,14 +1,49 @@
+const { loggerInfo, loggerError } = require("../utils/logging.js");
+
 exports.sendMessage = async (msg, client) => {
   const messageDetails = msg.content.substr(msg.content.indexOf(" ") + 1);
   const channelID = messageDetails.substr(0, messageDetails.indexOf(" "));
   const message = messageDetails.substr(messageDetails.indexOf(" ") + 1);
   const channel = client.channels.cache.get(channelID);
 
-  if (channel) {
-    channel.send(message);
-    msg.react("👍");
-  } else {
-    msg.reply("Channel does not exist");
+  try {
+    if (channel) {
+      channel.send(message);
+      loggerInfo(
+        msg.author.username +
+          "#" +
+          msg.author.discriminator +
+          " sent " +
+          message +
+          " to channel " +
+          channelID
+      );
+      msg.react("👍");
+    } else {
+      msg.reply("Channel does not exist");
+      loggerError(
+        msg.author.username +
+          "#" +
+          msg.author.discriminator +
+          " tried to send " +
+          message +
+          " to channel " +
+          channelID +
+          " which does not exist",
+        "channel" + channelID + " does not exist"
+      );
+    }
+  } catch (e) {
+    loggerError(
+      msg.author.username +
+        "#" +
+        msg.author.discriminator +
+        " could not send " +
+        message +
+        " to channel " +
+        channelID,
+      e
+    );
   }
 };
 
