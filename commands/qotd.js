7faa5e6b57c8774961773dccf101910fb4034caa
SaloniSS <sxs170015@utdallas.edu>
2021-04-05 18:15:08 -0500

    initialize qotd files

---
+++ commands/qotd.js
@@ -0,0 +1,6 @@
+const Discord = require("discord.js");
+const { loggerInfo, loggerError } = require("../utils/logging.js");
+
+const qotdRoleID = "828768969808674836";
+
+exports.sendQOTD = async (client, msg) => {};
